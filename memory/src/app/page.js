d4002f2d5c37d2c8e4ee22711797cead561d887d
annacano0 <anna.elisabet5@gmail.com>
2024-07-24 11:29:20 +0200

    FEATURE: Starting the game - Default board of 3x4

--- memory/src/app/page.js
+++ memory/src/app/page.js
@@ -1,7 +1,10 @@
+import Game from '../components/Game.jsx'
+
 export default function Home () {
   return (
     <>
       <h1 data-testid='app-title'>hello :)</h1>
+      <Game />
     </>
   )
 }
---
+++ memory/src/components/Game.jsx
@@ -0,0 +1,24 @@
+export default function Game () {
+  return (
+    <>
+      <div data-test-id='board-row'>
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+      </div>
+      <div data-test-id='board-row'>
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+      </div>
+      <div data-test-id='board-row'>
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+        <div data-testid='board-card' />
+      </div>
+    </>
+  )
+}
