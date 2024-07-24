af80727784d80432b35ce81ace1b1e038ba212a3
annacano0 <anna.elisabet5@gmail.com>
2024-07-24 12:27:28 +0200

    FEATURE: Starting the game - All cards should be unflipped

--- memory/src/components/Game.jsx
+++ memory/src/components/Game.jsx
@@ -2,22 +2,22 @@ export default function Game () {
   return (
     <>
       <div data-test-id='board-row'>
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
       </div>
       <div data-test-id='board-row'>
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
       </div>
       <div data-test-id='board-row'>
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
-        <div data-testid='board-card' />
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
+        <div data-testid='board-card' className="unflipped"/>
       </div>
     </>
   )
